9793934403a6ddacdaf2bbe0877ed833e45f5fd9
perlancar (on pc-home) <perlancar@gmail.com>
2021-06-01 16:35:52 +0700

    Tweak formatting

--- lib/Acme/CPANModules/NO_COLOR.pm
+++ lib/Acme/CPANModules/NO_COLOR.pm
@@ -9,7 +9,7 @@ our $LIST = {
     summary => "Modules that follow the NO_COLOR convention",
     description => <<'_',
 
-The NO_COLOR convention (see https://no-color.org) lets user disable color
+The NO_COLOR convention (see <https://no-color.org>) lets user disable color
 output of console programs by defining an environment variable called NO_COLOR.
 The existence of said environment variable, regardless of its value, signals
 that programs should not use colored output.
